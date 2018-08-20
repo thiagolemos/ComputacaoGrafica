1f8819312b1b81a8486cf6839a3e100b6c8daa77
Thiago Lemos <thiagolemos95@outlook.com>
2018-08-20 20:03:01 -0300

    Add project files.

---
+++ ComputacaoGrafica/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace ComputacaoGrafica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.x1 = new System.Windows.Forms.TextBox();
+            this.y1 = new System.Windows.Forms.TextBox();
+            this.y2 = new System.Windows.Forms.TextBox();
+            this.x2 = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.Metodo = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.anguloText = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.forceText = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.labelResultado = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.imagem = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "P1 (             ,             )";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(8, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "P2 (             ,             )";
+            // 
+            // x1
+            // 
+            this.x1.Location = new System.Drawing.Point(35, 6);
+            this.x1.Name = "x1";
+            this.x1.Size = new System.Drawing.Size(30, 20);
+            this.x1.TabIndex = 2;
+            // 
+            // y1
+            // 
+            this.y1.Location = new System.Drawing.Point(77, 6);
+            this.y1.Name = "y1";
+            this.y1.Size = new System.Drawing.Size(30, 20);
+            this.y1.TabIndex = 3;
+            // 
+            // y2
+            // 
+            this.y2.Location = new System.Drawing.Point(77, 35);
+            this.y2.Name = "y2";
+            this.y2.Size = new System.Drawing.Size(30, 20);
+            this.y2.TabIndex = 5;
+            // 
+            // x2
+            // 
+            this.x2.Location = new System.Drawing.Point(35, 35);
+            this.x2.Name = "x2";
+            this.x2.Size = new System.Drawing.Size(30, 20);
+            this.x2.TabIndex = 4;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(306, 64);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(121, 23);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Metodo
+            // 
+            this.Metodo.FormattingEnabled = true;
+            this.Metodo.Items.AddRange(new object[] {
+            "Translação",
+            "Escala",
+            "Rotação",
+            "Reflexão (Eixo X)",
+            "Reflexão (Eixo Y)",
+            "Reflexão (Origem)",
+            "Cisalhamento Vertical",
+            "Cisalhamento Horizontal"});
+            this.Metodo.Location = new System.Drawing.Point(306, 30);
+            this.Metodo.Name = "Metodo";
+            this.Metodo.Size = new System.Drawing.Size(121, 21);
+            this.Metodo.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(126, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Ponto de Origem";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(126, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Ponto de Translação e Escala";
+            // 
+            // anguloText
+            // 
+            this.anguloText.Location = new System.Drawing.Point(35, 64);
+            this.anguloText.Name = "anguloText";
+            this.anguloText.Size = new System.Drawing.Size(72, 20);
+            this.anguloText.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(126, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Ângulo da rotação";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 67);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "ɵ";
+            // 
+            // forceText
+            // 
+            this.forceText.Location = new System.Drawing.Point(35, 90);
+            this.forceText.Name = "forceText";
+            this.forceText.Size = new System.Drawing.Size(72, 20);
+            this.forceText.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 93);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "F";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(126, 93);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(158, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Força aplicada ao Cisalhamento";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(303, 6);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(54, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Operação";
+            // 
+            // labelResultado
+            // 
+            this.labelResultado.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelResultado.Location = new System.Drawing.Point(11, 147);
+            this.labelResultado.Name = "labelResultado";
+            this.labelResultado.Size = new System.Drawing.Size(482, 165);
+            this.labelResultado.TabIndex = 17;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(16, 125);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(55, 13);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "Resultado";
+            // 
+            // imagem
+            // 
+            this.imagem.BackColor = System.Drawing.Color.White;
+            this.imagem.Location = new System.Drawing.Point(11, 147);
+            this.imagem.Name = "imagem";
+            this.imagem.Size = new System.Drawing.Size(486, 170);
+            this.imagem.TabIndex = 19;
+            this.imagem.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 329);
+            this.Controls.Add(this.imagem);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.labelResultado);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.forceText);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.anguloText);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Metodo);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.y2);
+            this.Controls.Add(this.x2);
+            this.Controls.Add(this.y1);
+            this.Controls.Add(this.x1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Transformações Geométricas em 2D";
+            ((System.ComponentModel.ISupportInitialize)(this.imagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox x1;
+        private System.Windows.Forms.TextBox y1;
+        private System.Windows.Forms.TextBox y2;
+        private System.Windows.Forms.TextBox x2;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.ComboBox Metodo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox anguloText;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox forceText;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label labelResultado;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.PictureBox imagem;
+    }
+}
+
---
+++ ComputacaoGrafica/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace ComputacaoGrafica
+{
+    public partial class Form1 : Form
+    {
+        Bitmap areaDesenho;
+        Color corPreenche;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            areaDesenho = new Bitmap(imagem.Size.Width, imagem.Size.Height);
+            corPreenche = Color.Black;
+        }
+
+        int[,] Resultado = new int[3, 3];
+        double[,] ResultRotacao = new double[3, 3];
+        int[,] MatrizTranslacao = new int[3, 3] { { 1, 0, 9999 }, { 0, 1, 9999 }, { 0, 0, 1 } };
+        int[,] MatrizEscala = new int[3, 3] { { 9999, 0, 0 }, { 0, 9999, 0 }, { 0, 0, 1 } };
+        int[,] MatrizReflexaoX = new int[3, 3] { { 1, 0, 0 }, { 0, -1, 0 }, { 0, 0, 1 } };
+        int[,] MatrizReflexaoY = new int[3, 3] { { -1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } };
+        int[,] MatrizReflexaoOrigem = new int[3, 3] { { -1, 0, 0 }, { 0, -1, 0 }, { 0, 0, 1 } };
+        int[,] MatrizCisalhamentoVertical = new int[3, 3] { { 1, 0, 0 }, { 9999, 1, 0 }, { 0, 0, 1 } };
+        int[,] MatrizCisalhamentoHorizontal = new int[3, 3] { { 1, 9999, 0 }, { 0, 1, 0 }, { 0, 0, 1 } };
+        double[,] MatrizRotacao = new double[3, 3] { { 9999, 8888, 0 }, { 8888, 9999, 0 }, { 0, 0, 1 } };
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (x1.Text == "" || y1.Text == "" || anguloText.Text == "" || x2.Text == "" || y2.Text == "" || forceText.Text == "")
+            {
+                MessageBox.Show("Favor informar todos os campos !");
+                return;
+            }
+
+            int[,] Coef = new int[3, 1];
+            Coef[0, 0] = int.Parse(x1.Text);
+            Coef[1, 0] = int.Parse(y1.Text);
+            Coef[2, 0] = 1;
+
+            //Esta dando erro nessa conversão ao executar o codigo
+            double cos = Math.Cos(Double.Parse(anguloText.Text));
+            double sen = Math.Sin(Convert.ToDouble(anguloText.Text));
+
+            int T1 = Convert.ToInt32(x2.Text);
+            int T2 = Convert.ToInt32(y2.Text);
+            int F = Convert.ToInt32(forceText.Text);
+
+            areaDesenho.SetPixel(int.Parse(x1.Text), int.Parse(y1.Text), corPreenche);
+            areaDesenho.SetPixel(int.Parse(x2.Text), int.Parse(y2.Text), corPreenche);
+
+            switch (Metodo.Text.ToString())
+            {
+                case "Translação":
+                    Resultado = Translacao(MatrizTranslacao, Coef, T1, T2);
+                    ApresentarResultado(Resultado);
+                    break;
+                case "Escala":
+                    Resultado = Escala(MatrizEscala, Coef, T1, T2);
+                    ApresentarResultado(Resultado);
+                    break;
+                case "Rotação":
+                    ResultRotacao = Rotacao(MatrizRotacao, Coef, sen, cos);
+                    ApresentarResultado(ResultRotacao);
+                    break;
+                case "Reflexão(Eixo X)":
+                    Resultado = ReflexaoX(MatrizReflexaoX, Coef);
+                    ApresentarResultado(Resultado);
+                    break;
+                case "Reflexão(Eixo Y)":
+                    Resultado = ReflexaoY(MatrizReflexaoY, Coef);
+                    ApresentarResultado(Resultado);
+                    break;
+                case "Reflexão(Origem)":
+                    Resultado = ReflexaoOrigem(MatrizReflexaoOrigem, Coef);
+                    ApresentarResultado(Resultado);
+                    break;
+                case "Cisalhamento Vertical":
+                    Resultado = CisalhamentoVertical(MatrizCisalhamentoVertical, Coef, F);
+                    ApresentarResultado(Resultado);
+                    break;
+                case "Cisalhamento Horizontal":
+                    Resultado = CisalhamentoHorizontal(MatrizCisalhamentoHorizontal, Coef, F);
+                    ApresentarResultado(Resultado);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        public static int[,] Translacao(int[,] A, int[,] Coef, int T1, int T2)
+        {
+            int[,] C = new int[3, 1];
+
+            A[0, 2] = T1;
+            A[1, 2] = T2;
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 1; j++)
+                    for (int k = 0; k < 3; k++)
+                    {
+                        C[i, j] += A[i, k] * Coef[k, j];
+                    }
+
+            return C;
+        }
+
+        public static int[,] Escala(int[,] A, int[,] Coef, int T1, int T2)
+        {
+            int[,] C = new int[3, 1];
+
+            A[0, 0] = T1;
+            A[1, 1] = T2;
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 1; j++)
+                    for (int k = 0; k < 3; k++)
+                    {
+                        C[i, j] += A[i, k] * Coef[k, j];
+                    }
+
+            return C;
+        }
+
+        public static double[,] Rotacao(double[,] A, int[,] Coef, double seno, double cosseno)
+        {
+            double[,] C = new double[3, 1];
+
+            A[0, 0] = cosseno;
+            A[0, 1] = seno * (-1);
+            A[1, 0] = seno;
+            A[1, 1] = cosseno;
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 1; j++)
+                    for (int k = 0; k < 3; k++)
+                        C[i, j] += A[i, k] * Convert.ToDouble(Coef[k, j]);
+
+            return C;
+        }
+
+        public static int[,] ReflexaoX(int[,] A, int[,] Coef)
+        {
+            int[,] C = new int[3, 1];
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 1; j++)
+                    for (int k = 0; k < 3; k++)
+                    {
+                        C[i, j] += A[i, k] * Coef[k, j];
+                    }
+
+            return C;
+        }
+
+        public static int[,] ReflexaoY(int[,] A, int[,] Coef)
+        {
+            int[,] C = new int[3, 1];
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 1; j++)
+                    for (int k = 0; k < 3; k++)
+                    {
+                        C[i, j] += A[i, k] * Coef[k, j];
+                    }
+
+            return C;
+        }
+
+        public static int[,] ReflexaoOrigem(int[,] A, int[,] Coef)
+        {
+            int[,] C = new int[3, 1];
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 1; j++)
+                    for (int k = 0; k < 3; k++)
+                    {
+                        C[i, j] += A[i, k] * Coef[k, j];
+                    }
+
+            return C;
+        }
+
+        public static int[,] CisalhamentoVertical(int[,] A, int[,] Coef, int F)
+        {
+            int[,] C = new int[3, 1];
+
+            A[0, 1] = F;
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 1; j++)
+                    for (int k = 0; k < 3; k++)
+                    {
+                        C[i, j] += A[i, k] * Coef[k, j];
+                    }
+
+            return C;
+        }
+
+        public static int[,] CisalhamentoHorizontal(int[,] A, int[,] Coef, int F)
+        {
+            int[,] C = new int[3, 1];
+
+            A[1, 0] = F;
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 1; j++)
+                    for (int k = 0; k < 3; k++)
+                    {
+                        C[i, j] += A[i, k] * Coef[k, j];
+                    }
+
+            return C;
+        }
+
+        //Falta implementar a função para apresentar o resultado na labelResultado
+        public void ApresentarResultado(int[,] Result)
+        {
+            //string resultado = "";
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    for (int j = 0; j < 1; j++)
+            //    {
+            //        resultado += String.Format(@"[{0},{1}] {2}"
+            //                                    , i
+            //                                    , j
+            //                                    , Result[i, j]);
+            //        resultado += "\n";
+
+            //        areaDesenho.SetPixel(i, j, corPreenche);
+            //    }
+            //}
+
+            areaDesenho.SetPixel(Result[0, 0], Result[1, 0], corPreenche);
+
+            //labelResultado.Text = resultado;
+
+            //areaDesenho.SetPixel(x, y, corPreenche);
+            imagem.Image = areaDesenho;
+
+        }
+
+        public void ApresentarResultado(double[,] Result)
+        {
+            //A impresão da função de rotação recebe double por causa das operaçoes com seno e cosseno
+        }
+
+    }
+}
